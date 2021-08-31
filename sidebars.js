a612ed162e9207cb7200d34f92139bb8b8183761
ying <ying.xu@cryptape.com>
2021-08-31 09:47:50 +0800

    deploy godwoken version 1

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
 	sidebar2: [	
 	{'CKB SDKs': ['introduction/overview_go','introduction/overview_java']},
-	{'Godwoken': ['introduction/godwoken']},
+	{'Godwoken': ['introduction/godwoken','introduction/deploy']},
   ],
 };
